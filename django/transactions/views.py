9447e5e1b7f9781ba37e052387e6315bd1030522
bamjun <khy5116@naver.com>
2024-07-04 20:58:23 +0900

    test

--- django/transactions/views.py
+++ django/transactions/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
-from solana.rpc.api import Client
 from solders.pubkey import Pubkey
+from solana.rpc.async_api import AsyncClient
+import asyncio
 
 def index(request):
     return render(request, 'index.html')
 
-def get_transactions(request):
-    client = Client("https://api.mainnet-beta.solana.com")
+async def fetch_transactions():
+    client = AsyncClient("https://api.mainnet-beta.solana.com")
     public_key = "MoonCVVNZFSYkqNXP6bxHLPL6QQJiMagDL3qcqUQTrG"
     public_key_obj = Pubkey.from_string(public_key)
     
-    # Solana 계정의 최근 트랜잭션 기록 조회
-    limit = 5
+    limit = 10
     transactions_resp = client.get_signatures_for_address(public_key_obj, limit=limit)
-    transactions = transactions_resp.value  # 올바른 접근 방식 사용
+    transactions = transactions_resp
+
+    
+    await client.close()
+    return transactions
+
+def get_transactions(request):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    transactions = loop.run_until_complete(fetch_transactions())
     
     return render(request, 'transactions.html', {'transactions': transactions})
